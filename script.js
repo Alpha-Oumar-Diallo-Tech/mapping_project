383bb5c4b94fe7ecc357be97fdbc591b35b957fe
Alpha Oumar Diallo <alpha629844429@gmail.com>
2024-10-29 11:04:07 +0000

    aod-modal

--- script.js
+++ script.js
@@ -13,15 +13,13 @@ class Infrastructure {
 
 class App {
     #map
+    #marqueur
     #map_event
     #infrastructure
     #infrastructure_data = new Array ()
     constructor () {
         this.showMap ()
-        this._get_initial_app_data ("hospital_data.json")
-        // this.addMarker(9.6412, -13.5784, 'agence', 'Agence Ecobank');
-        // this.addMarker(9.6512, -13.5684, 'pointXpress', 'Point Xpress');
-        // this.addMarker(9.6612, -13.5584, 'ecole', 'École Primaire');
+        this.get_all_initial_data ()
     }
     showMap () {
         const guinea = [10.973549898080215, -10.973549898080215]
@@ -30,6 +28,9 @@ class App {
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.#map);
+    }
+    get_all_initial_data () {
+        this._get_initial_app_data ("hospital_data.json")
     }
     async _get_initial_app_data (json_file) {
         try {
@@ -43,70 +44,87 @@ class App {
             console.log (error)
         }
     }
+    popupContent (name, category) {
+        let my_popup_content = `
+            <div class = "popup_container">
+                <h1 class = "popup_title">${name}</h1>
+                <span class = "popup_categorie">${category}</span>
+                <button class = "popup_button">en savoir plus</button>
+            </div>
+        `
+        // const container = document.querySelector (".leaflet-popup-content-wrapper")
+        // console.log (container)
+        // const pops = container.closest (".leaflet-popup-content-wrapper")
+        // pops.style.color = "black"
+        // this.#marqueur.on ("popupopen", function () {
+        //     const pop = document.querySelector (".leaflet-popup-content-wrapper")
+        //     if (container) {
+        //         switch (category) {
+        //             case "hospital":
+        //                 document.querySelector (".leaflet-popup-content-wrapper").style.color = "black"
+        //                 return my_popup_content
+        //                 // break;
+        //             default:
+        //                 break;
+        //         }
+        //     }
+        // })
+        
+        // switch (category) {
+        //     case "hospital":
+        //         document.querySelector (".leaflet-popup-content-wrapper").style.color = "black"
+        //         return my_popup_content
+        //         // break;
+        //     default:
+        //         break;
+        // }
+        return my_popup_content
+    }
     showIndicator (infras) {
-        const customIcon = L.ExtraMarkers.icon({
-            icon: 'fa-coffee',
-            markerColor: 'red',
-            shape: 'square',
-            prefix: 'fa'
-        });
         infras.forEach(infras => {
             const {name, category, coordinates : {latitude, longitude}} = infras
-            const pop = `
-            <div class = "pop">
-                <h1 class = "title">${name}</h1>
-                <p class = "description">${category}</p>
-                <buttton class = "btn">en savoir plus</buttton>
-            </div>`
-            L.marker([latitude, longitude], {icon: customIcon}).addTo(this.#map)
-            .bindPopup(pop, {
-                autoClose: false,
-                closeOnClick: false
-            })
+
+            const my_popup_content = this.popupContent (name, category)
+            const marqueur = L.marker([latitude, longitude]).addTo(this.#map)
+            .bindPopup(my_popup_content,
+                {
+                    autoClose: false,
+                    closeOnClick: false
+                }
+            )
             .openPopup();
+            // switch (category) {
+            //     case "hospital":
+            //         pop.style.backgroundColor  = "black";
+            //         break;
+            //     case "university":
+            //         pop.style.backgroundColor  = "blue";
+            //         break;
+            //     default:
+            //         pop.style.backgroundColor  = "initial";
+            //         break;
+            // }
+            // marqueur.on("popupopen", (e) => {
+            //     const pop = e.popup._wrapper; // Accède directement à l'élément du popup via l'événement
+            //     if (pop) {
+            //         switch (category) {
+            //             case "hospital":
+            //                 pop.style.backgroundColor  = "black";
+            //                 break;
+            //             case "university":
+            //                 pop.style.backgroundColor  = "blue";
+            //                 break;
+            //             default:
+            //                 pop.style.backgroundColor  = "initial";
+            //                 break;
+            //         }
+            //     }
+            // });
         });
+        
     }
-    // Fonction pour créer une icône personnalisée en fonction du type
-    createCustomIcon(type) {
-        let iconOptions = {
-            icon: 'fa-circle', // icône par défaut
-            markerColor: 'blue', // couleur par défaut
-            prefix: 'fa', // préfixe pour Font Awesome
-        };
-
-        switch (type) {
-            case 'ecole':
-                iconOptions.icon = 'fa-school';
-                iconOptions.markerColor = 'purple';
-            break;
-            case 'hopital':
-                iconOptions.icon = 'fa-hospital';
-                iconOptions.markerColor = 'red';
-            break;
-            case 'bibliotheque':
-                iconOptions.icon = 'fa-book';
-                iconOptions.markerColor = 'blue';
-            break;
-            default:
-                iconOptions.icon = 'fa-map-marker';
-                iconOptions.markerColor = 'blue';
-        }
-        return L.AwesomeMarkers.icon(iconOptions);
-    }
-    // Fonction pour ajouter un marqueur avec une icône personnalisée
-    addMarker(lat, lng, type, popupContent) {
-    const customIcon = this.createCustomIcon(type);
-
-    L.marker([lat, lng], { icon: customIcon })
-        .addTo(map)
-        .bindPopup(popupContent);
-    }
-
-  // Exemple d'utilisation
-    // addMarker(9.6412, -13.5784, 'agence', 'Agence Ecobank');
-    // addMarker(9.6512, -13.5684, 'pointXpress', 'Point Xpress');
-    // addMarker(9.6612, -13.5584, 'ecole', 'École Primaire');
-
+    
+    
 }
 
 new App ()
